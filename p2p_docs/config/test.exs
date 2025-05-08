fc675c2c4ba2621eb32f8b6af0f58a356cb2bfdc
virgola <alessandro.debiasi@protonmail.com>
2025-05-08 08:25:23 +0200

    added ad-hoc config file for every env mix.env can have by default (eg. test, prod, dev)

---
+++ p2p_docs/config/test.exs
@@ -0,0 +1,35 @@
+import Config
+# This is the configuration file for the P2PDocs application.
+# It contains settings for the API server, network parameters, and other application-specific configurations.
+
+config :p2p_docs, :vector_clock,
+  # The module to use for vector clock implementation
+  module: P2PDocs.Network.NaiveVectorClock
+
+# CausalBroadcast configuration (injecting the module to use)
+config :p2p_docs, :causal_broadcast,
+  module: P2PDocs.Network.CausalBroadcast,
+  ets_table: :causal_broadcast_state
+
+# NeighborHandler configuration
+config :p2p_docs, :neighbor_handler,
+  module: P2PDocs.Network.NeighborHandlerMock,
+  ets_table: :neighbor_handler_state,
+  port: 45892,
+  mcast_addr: {224, 1, 1, 1},
+  mcast_if: {192, 168, 1, 1},
+  iface: {0, 0, 0, 0},
+  ttl: 4,
+  interval: 5_000,
+  secret: "default"
+
+# CRDT Manager configuration
+config :p2p_docs, :crdt_manager,
+  module: P2PDocs.CRDT.Manager,
+  ets_table: :crdt_manager_state
+
+# API server configuration
+config :p2p_docs, :api,
+  module: P2PDocs.API.Server,
+  ets_table: :api_server_state,
+  port: 4000
